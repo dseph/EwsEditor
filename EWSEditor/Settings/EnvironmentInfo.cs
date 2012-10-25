71a4b60c8a9eddfd6297ba55ea986fc2888597a9
mstehle_cp <SND\mstehle_cp>
2012-10-25 14:12:57 +0000

    Not many changes needed.

--- EWSEditor/Settings/EnvironmentInfo.cs
+++ EWSEditor/Settings/EnvironmentInfo.cs
@@ -17,10 +17,10 @@ namespace EWSEditor.Settings
 
     internal class EnvironmentInfo
     {
-        internal const string BuiltForEwsManagedApiVersion = "14.03.0067.001";
+        internal const string BuiltForEwsManagedApiVersion = "15.00.0516.014";
         internal const string Framework35RegistryPath = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.5";
-        internal const string EwsManagedApiInstallPath = @"C:\Program Files\Microsoft\Exchange\Web Services\1.2\Microsoft.Exchange.WebServices.dll";
-        internal const string EwsManagedApiDownloadUrl = "http://www.microsoft.com/en-us/download/details.aspx?id=30141";
+        internal const string EwsManagedApiInstallPath = @"C:\Program Files\Microsoft\Exchange\Web Services\2.0\Microsoft.Exchange.WebServices.dll";
+        internal const string EwsManagedApiDownloadUrl = "http://www.microsoft.com/en-us/download/details.aspx?id=35371";
         internal const string EwsEditorProjectUrl = "http://ewseditor.codeplex.com";
         internal const string EwsEditorBlogPostsUrl = "http://blogs.msdn.com/mstehle/archive/tags/EWSEditor/default.aspx";
 
