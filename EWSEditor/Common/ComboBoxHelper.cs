37a2315e30b66aba29772e89dc4024e1c2c8ec06
Dseph_cp <SND\Dseph_cp>
2013-07-09 22:17:30 +0000

    The timezone can now optionally be set on login. I merged-in a lot of calendaring and diagnostics code I wrote for a parallel app. However, it is not yet accessible from the menu. Updated Timezone info screen.

---
+++ EWSEditor/Common/ComboBoxHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using EWSEditor.Common;
+
+namespace EWSEditor.Common 
+{
+    public class ComboBoxHelper
+    {
+
+        static public void AddEnumsToComboBox(ref ComboBox oComboBox, Type oType, bool SetFirstAsDefault)
+        {
+
+            AddEnumsToComboBox(ref oComboBox, oType);
+
+            if (SetFirstAsDefault == true)
+            {
+                oComboBox.Text = oComboBox.Items[0].ToString();
+            }
+
+        }
+
+        static public void AddEnumsToComboBox(ref ComboBox oComboBox, Type oType)
+        {
+            oComboBox.Items.Clear();
+
+            Array arrItems = System.Enum.GetValues(oType);
+
+            foreach (int iItem in arrItems)
+            {
+
+                string sItem = Enum.GetName(oType, iItem);
+                oComboBox.Items.Add(sItem);
+            }
+        }
+    }
+}
