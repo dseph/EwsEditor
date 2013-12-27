51ed2e60fd8b8a9f4154197190f804f29c87ad14
Dseph_cp <SND\Dseph_cp>
2013-12-27 17:11:44 +0000

    Started 1.7.4

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.3.*")]
-[assembly: AssemblyFileVersion("1.7.3")]
+[assembly: AssemblyVersion("1.7.4.*")]
+[assembly: AssemblyFileVersion("1.7.4")]
