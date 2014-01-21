664ca5ce8f4dd48d014bc8bfd60efea9213594ac
Dseph_cp <SND\Dseph_cp>
2014-01-21 22:59:39 +0000

    Fixed a bug with authentication in the ews post window - have to do a new release.

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.6.*")]
-[assembly: AssemblyFileVersion("1.7.6")]
+[assembly: AssemblyVersion("1.7.5.*")]
+[assembly: AssemblyFileVersion("1.7.5")]
