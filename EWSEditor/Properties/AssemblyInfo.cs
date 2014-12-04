5889e5c00105d524e37c1152d2a2be9800007b39
Dseph_cp <dseph@users.noreply.github.com>
2014-12-04 01:50:14 +0000

    Check-in for 1.11

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.10.0.*")]
-[assembly: AssemblyFileVersion("1.10.0")]
+[assembly: AssemblyVersion("1.11.0.*")]
+[assembly: AssemblyFileVersion("1.11.0")]
