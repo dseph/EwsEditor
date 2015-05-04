fc851409b3ff61f42cd334e9fc082c8bc1a427b5
Dseph_cp <SND\Dseph_cp>
2015-05-04 18:50:22 +0000

    Check-in for 1.13

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.12.0.*")]
-[assembly: AssemblyFileVersion("1.12.0")]
+[assembly: AssemblyVersion("1.13.0.*")]
+[assembly: AssemblyFileVersion("1.13.0")]
