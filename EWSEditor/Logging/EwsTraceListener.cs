aeea5487b8a90a3897bb7a452323f39008eb1ff3
dseph <dseph@users.noreply.github.com>
2017-07-07 12:17:49 -0400

    Changed logging of request and response headers so that each header will display on a seperate line in the log viewer. It was a matter of changing \n to \r\n so that the control would display the text on seperate lines.

--- EWSEditor/Logging/EwsTraceListener.cs
+++ EWSEditor/Logging/EwsTraceListener.cs
@@ -29,9 +29,26 @@ namespace EWSEditor.Logging
                 }
             }
 
-            DebugLog.WriteEwsLog(
+            if (traceType == "EwsResponseHttpHeaders" || traceType == "EwsRequestHttpHeaders")
+            {
+                // Make it easier to read the headers returned by changing \n to \r\n.  This will make it easier to view in the log viewer.
+                string sCleaned_traceMessage = string.Empty;
+                sCleaned_traceMessage = traceMessage.Replace("\r\n", "\n");
+                sCleaned_traceMessage = sCleaned_traceMessage.Replace("\n", "\r\n");
+                //"<Trace Tag=\"EwsRequestHttpHeaders\" Tid=\"1\" Time=\"2017-07-07 16:06:43Z\">\r\nPOST /EWS/Exchange.asmx HTTP/1.1\r\nContent-Type: text/xml; charset=utf-8\r\nAccept: text/xml\r\nUser-Agent: EWSEditor (ExchangeServicesClient/0.0.0.0)\r\nAccept-Encoding: gzip,deflate\r\nclient-request-id: c3c5dc97-9433-4b15-bfa1-8867e96e2cfd\r\nreturn-client-request-id: true\r\n\r\n\r\n</Trace>\r\n"
+                sCleaned_traceMessage = sCleaned_traceMessage.Replace("\r\n\r\n\r\n</Trace>", "\r\n</Trace>");
+                sCleaned_traceMessage = sCleaned_traceMessage.Replace("\r\n\r\n</Trace>", "\r\n</Trace>");
+                DebugLog.WriteEwsLog(
+                    traceType,
+                    sCleaned_traceMessage);
+ 
+            }
+            else
+            {
+                DebugLog.WriteEwsLog(
                 traceType,
                 traceMessage);
+            }
         }
 
         private static bool TryGetResponseHeader(string traceMessage, string headerName, out string headerValue)
