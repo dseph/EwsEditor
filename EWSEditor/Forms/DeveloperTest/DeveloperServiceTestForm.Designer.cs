9fb65a7073f50cd506a21774037d8945a4a3f6e6
dseph <dseph@users.noreply.github.com>
2017-10-31 13:29:02 -0400

    preparing for release - cleaning.

--- EWSEditor/Forms/DeveloperTest/DeveloperServiceTestForm.Designer.cs
+++ EWSEditor/Forms/DeveloperTest/DeveloperServiceTestForm.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.btnTest = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnTest
@@ -60,23 +59,11 @@
     " may test their EWS Managed API code to work on the initialized ExchangeService " +
     "object.";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(403, 299);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 46);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // DeveloperServiceTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 380);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnTest);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -92,6 +79,5 @@
 
         private System.Windows.Forms.Button btnTest;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
