76ae84edba8d2fa429da1c92cc3ed18887067e9e
Daniel Bagley <dseph2000@hotmail.com>
2021-05-07 18:27:49 -0400

    Fixed spelling - Address in one instance was not correctly spelled.

--- EWSEditor/Forms/Dialogs/FolderIdDialog.Designer.cs
+++ EWSEditor/Forms/Dialogs/FolderIdDialog.Designer.cs
@@ -60,11 +60,11 @@
             this.WellKnownGroup.Controls.Add(this.label12);
             this.WellKnownGroup.Controls.Add(this.MailboxAddressText);
             this.WellKnownGroup.Controls.Add(this.label11);
-            this.WellKnownGroup.Location = new System.Drawing.Point(32, 176);
-            this.WellKnownGroup.Margin = new System.Windows.Forms.Padding(4);
+            this.WellKnownGroup.Location = new System.Drawing.Point(48, 275);
+            this.WellKnownGroup.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.WellKnownGroup.Name = "WellKnownGroup";
-            this.WellKnownGroup.Padding = new System.Windows.Forms.Padding(4);
-            this.WellKnownGroup.Size = new System.Drawing.Size(608, 193);
+            this.WellKnownGroup.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.WellKnownGroup.Size = new System.Drawing.Size(912, 302);
             this.WellKnownGroup.TabIndex = 3;
             this.WellKnownGroup.TabStop = false;
             this.WellKnownGroup.Text = "Well Known Folder Name";
@@ -72,10 +72,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 94);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 147);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(360, 17);
+            this.label1.Size = new System.Drawing.Size(548, 25);
             this.label1.TabIndex = 5;
             this.label1.Text = "SMTP address for the mailbox to do delegate access to:";
             // 
@@ -83,59 +83,59 @@
             // 
             this.TempWellKnownFolderCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.TempWellKnownFolderCombo.FormattingEnabled = true;
-            this.TempWellKnownFolderCombo.Location = new System.Drawing.Point(130, 34);
-            this.TempWellKnownFolderCombo.Margin = new System.Windows.Forms.Padding(4);
+            this.TempWellKnownFolderCombo.Location = new System.Drawing.Point(195, 53);
+            this.TempWellKnownFolderCombo.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.TempWellKnownFolderCombo.Name = "TempWellKnownFolderCombo";
-            this.TempWellKnownFolderCombo.Size = new System.Drawing.Size(448, 24);
+            this.TempWellKnownFolderCombo.Size = new System.Drawing.Size(670, 33);
             this.TempWellKnownFolderCombo.TabIndex = 1;
             this.TempWellKnownFolderCombo.SelectedIndexChanged += new System.EventHandler(this.TempWellKnownFolderCombo_SelectedIndexChanged);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(8, 44);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(12, 69);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(93, 17);
+            this.label8.Size = new System.Drawing.Size(141, 25);
             this.label8.TabIndex = 0;
             this.label8.Text = "Folder Name:";
             // 
             // label12
             // 
-            this.label12.Location = new System.Drawing.Point(29, 146);
-            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Location = new System.Drawing.Point(44, 228);
+            this.label12.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(549, 42);
+            this.label12.Size = new System.Drawing.Size(824, 66);
             this.label12.TabIndex = 2;
             this.label12.Text = "Enter the SMTP address of the mailbox to access using delegate access.  Leave thi" +
     "s field empty to access the default mailbox.  ";
             // 
             // MailboxAddressText
             // 
-            this.MailboxAddressText.Location = new System.Drawing.Point(156, 117);
-            this.MailboxAddressText.Margin = new System.Windows.Forms.Padding(4);
+            this.MailboxAddressText.Location = new System.Drawing.Point(234, 183);
+            this.MailboxAddressText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.MailboxAddressText.Name = "MailboxAddressText";
-            this.MailboxAddressText.Size = new System.Drawing.Size(422, 22);
+            this.MailboxAddressText.Size = new System.Drawing.Size(631, 31);
             this.MailboxAddressText.TabIndex = 4;
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(29, 121);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(44, 189);
+            this.label11.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(98, 17);
+            this.label11.Size = new System.Drawing.Size(162, 25);
             this.label11.TabIndex = 3;
-            this.label11.Text = "SMTP Adress:";
+            this.label11.Text = "SMTP Address:";
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(531, 496);
-            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Location = new System.Drawing.Point(796, 775);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.Size = new System.Drawing.Size(150, 44);
             this.btnCancel.TabIndex = 5;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -145,10 +145,10 @@
             // 
             this.OkButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.OkButton.Location = new System.Drawing.Point(424, 496);
-            this.OkButton.Margin = new System.Windows.Forms.Padding(4);
+            this.OkButton.Location = new System.Drawing.Point(636, 775);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.OkButton.Name = "OkButton";
-            this.OkButton.Size = new System.Drawing.Size(100, 28);
+            this.OkButton.Size = new System.Drawing.Size(150, 44);
             this.OkButton.TabIndex = 4;
             this.OkButton.Text = "OK";
             this.OkButton.UseVisualStyleBackColor = true;
@@ -158,11 +158,11 @@
             // 
             this.UniqueIdGroup.Controls.Add(this.SpecFolderIdText);
             this.UniqueIdGroup.Controls.Add(this.label9);
-            this.UniqueIdGroup.Location = new System.Drawing.Point(32, 53);
-            this.UniqueIdGroup.Margin = new System.Windows.Forms.Padding(4);
+            this.UniqueIdGroup.Location = new System.Drawing.Point(48, 83);
+            this.UniqueIdGroup.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.UniqueIdGroup.Name = "UniqueIdGroup";
-            this.UniqueIdGroup.Padding = new System.Windows.Forms.Padding(4);
-            this.UniqueIdGroup.Size = new System.Drawing.Size(586, 70);
+            this.UniqueIdGroup.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.UniqueIdGroup.Size = new System.Drawing.Size(879, 109);
             this.UniqueIdGroup.TabIndex = 1;
             this.UniqueIdGroup.TabStop = false;
             this.UniqueIdGroup.Text = "Unique ID";
@@ -170,28 +170,28 @@
             // SpecFolderIdText
             // 
             this.SpecFolderIdText.Enabled = false;
-            this.SpecFolderIdText.Location = new System.Drawing.Point(133, 25);
-            this.SpecFolderIdText.Margin = new System.Windows.Forms.Padding(4);
+            this.SpecFolderIdText.Location = new System.Drawing.Point(200, 39);
+            this.SpecFolderIdText.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.SpecFolderIdText.Name = "SpecFolderIdText";
-            this.SpecFolderIdText.Size = new System.Drawing.Size(315, 22);
+            this.SpecFolderIdText.Size = new System.Drawing.Size(470, 31);
             this.SpecFolderIdText.TabIndex = 1;
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(12, 32);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(18, 50);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(67, 17);
+            this.label9.Size = new System.Drawing.Size(102, 25);
             this.label9.TabIndex = 0;
             this.label9.Text = "Folder Id:";
             // 
             // UniqueIdRadio
             // 
-            this.UniqueIdRadio.Location = new System.Drawing.Point(16, 14);
-            this.UniqueIdRadio.Margin = new System.Windows.Forms.Padding(4);
+            this.UniqueIdRadio.Location = new System.Drawing.Point(24, 22);
+            this.UniqueIdRadio.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.UniqueIdRadio.Name = "UniqueIdRadio";
-            this.UniqueIdRadio.Size = new System.Drawing.Size(483, 30);
+            this.UniqueIdRadio.Size = new System.Drawing.Size(724, 47);
             this.UniqueIdRadio.TabIndex = 0;
             this.UniqueIdRadio.Text = "Identify folder by unique id.";
             this.UniqueIdRadio.UseVisualStyleBackColor = true;
@@ -199,10 +199,10 @@
             // 
             // WellKnownRadio
             // 
-            this.WellKnownRadio.Location = new System.Drawing.Point(16, 139);
-            this.WellKnownRadio.Margin = new System.Windows.Forms.Padding(4);
+            this.WellKnownRadio.Location = new System.Drawing.Point(24, 217);
+            this.WellKnownRadio.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.WellKnownRadio.Name = "WellKnownRadio";
-            this.WellKnownRadio.Size = new System.Drawing.Size(483, 30);
+            this.WellKnownRadio.Size = new System.Drawing.Size(724, 47);
             this.WellKnownRadio.TabIndex = 2;
             this.WellKnownRadio.Text = "Identify folder by well known name.";
             this.WellKnownRadio.UseVisualStyleBackColor = true;
@@ -210,10 +210,10 @@
             // 
             // rdoSelectFolder
             // 
-            this.rdoSelectFolder.Location = new System.Drawing.Point(16, 377);
-            this.rdoSelectFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.rdoSelectFolder.Location = new System.Drawing.Point(24, 589);
+            this.rdoSelectFolder.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rdoSelectFolder.Name = "rdoSelectFolder";
-            this.rdoSelectFolder.Size = new System.Drawing.Size(483, 30);
+            this.rdoSelectFolder.Size = new System.Drawing.Size(724, 47);
             this.rdoSelectFolder.TabIndex = 6;
             this.rdoSelectFolder.Text = "Identify folder by well known name.";
             this.rdoSelectFolder.UseVisualStyleBackColor = true;
@@ -221,9 +221,10 @@
             // 
             // btnPickFolder
             // 
-            this.btnPickFolder.Location = new System.Drawing.Point(6, 24);
+            this.btnPickFolder.Location = new System.Drawing.Point(9, 38);
+            this.btnPickFolder.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnPickFolder.Name = "btnPickFolder";
-            this.btnPickFolder.Size = new System.Drawing.Size(97, 23);
+            this.btnPickFolder.Size = new System.Drawing.Size(146, 36);
             this.btnPickFolder.TabIndex = 7;
             this.btnPickFolder.Text = "Pick Folder";
             this.btnPickFolder.UseVisualStyleBackColor = true;
@@ -231,20 +232,20 @@
             // 
             // txtPickedFolder
             // 
-            this.txtPickedFolder.Location = new System.Drawing.Point(185, 22);
-            this.txtPickedFolder.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPickedFolder.Location = new System.Drawing.Point(278, 34);
+            this.txtPickedFolder.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.txtPickedFolder.Name = "txtPickedFolder";
             this.txtPickedFolder.ReadOnly = true;
-            this.txtPickedFolder.Size = new System.Drawing.Size(403, 22);
+            this.txtPickedFolder.Size = new System.Drawing.Size(602, 31);
             this.txtPickedFolder.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(110, 27);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(165, 42);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.Size = new System.Drawing.Size(102, 25);
             this.label2.TabIndex = 9;
             this.label2.Text = "Folder Id:";
             // 
@@ -253,9 +254,11 @@
             this.groupBox1.Controls.Add(this.txtPickedFolder);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.btnPickFolder);
-            this.groupBox1.Location = new System.Drawing.Point(32, 414);
+            this.groupBox1.Location = new System.Drawing.Point(48, 647);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(608, 65);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(912, 102);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Pick Folder";
@@ -263,10 +266,10 @@
             // FolderIdDialog
             // 
             this.AcceptButton = this.OkButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(657, 537);
+            this.ClientSize = new System.Drawing.Size(986, 839);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.rdoSelectFolder);
             this.Controls.Add(this.WellKnownRadio);
@@ -275,7 +278,7 @@
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.OkButton);
             this.Controls.Add(this.WellKnownGroup);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Margin = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.Name = "FolderIdDialog";
             this.Text = "EWSEditor - Select a FolderId";
             this.Load += new System.EventHandler(this.FolderIdDialog_Load);
