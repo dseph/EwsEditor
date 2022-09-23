c0f94fbf40a27a0adf6ee0e2e0b1a2616040bb11
Daniel Bagley <dseph2000@hotmail.com>
2022-09-23 01:09:23 -0400

    Final checkin of EWS Editor 1.23

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.22.0.*")]
-[assembly: AssemblyFileVersion("1.22.0")]
+[assembly: AssemblyVersion("1.23.0.*")]
+[assembly: AssemblyFileVersion("1.23.0")]
