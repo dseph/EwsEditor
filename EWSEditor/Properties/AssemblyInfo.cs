d5552c9d9b27a3331b03ab5bb9eae2837609e31a
Daniel Bagley <dseph2000@hotmail.com>
2023-02-22 13:02:43 -0500

    updated version to 1.23.2 Note:  Testing out abilty to set the authority for oauth.

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EWSEditor - 1.23.1")]
+[assembly: AssemblyTitle("EWSEditor - 1.23.2")]
 [assembly: AssemblyDescription("Example code demonstrating EWS calls.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.23.1.*")]
-[assembly: AssemblyFileVersion("1.23.1")]
+[assembly: AssemblyVersion("1.23.2.*")]
+[assembly: AssemblyFileVersion("1.23.2")]
