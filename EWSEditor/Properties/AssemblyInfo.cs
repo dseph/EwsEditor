7c3e021c4fef2abee3ffce1a7981655cff9f51fb
Daniel Bagley <dseph2000@hotmail.com>
2025-02-19 14:04:58 -0500

    Correction 1.25.0 release

--- EWSEditor/Properties/AssemblyInfo.cs
+++ EWSEditor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EWSEditor - 1.26.1")]
+[assembly: AssemblyTitle("EWSEditor - 1.25.0")]
 [assembly: AssemblyDescription("Example code demonstrating EWS calls.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.26.0.*")]
-[assembly: AssemblyFileVersion("1.26.0")]
+[assembly: AssemblyVersion("1.25.0.*")]
+[assembly: AssemblyFileVersion("1.25.0")]
