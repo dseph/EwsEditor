82c9a402b474f8abe36f2fd17ce81e7fab1be1f2
Daniel Bagley <dseph2000@hotmail.com>
2025-10-15 09:54:08 -0400

    Added loadable .csv of X- headers

--- EWSEditor/Common/EwsApiTraceParser.cs
+++ EWSEditor/Common/EwsApiTraceParser.cs
@@ -6,94 +6,94 @@ using EWSEditor.Settings;
 
 namespace EWSEditor.Common
 {
-    internal class EwsApiTraceParser
-    {
-        private static string TraceLogBeginTag
-        {
-            get
-            {
-                string beginTag = string.Empty;
+    //internal class EwsApiTraceParser
+    //{
+    //    private static string TraceLogBeginTag
+    //    {
+    //        get
+    //        {
+    //            string beginTag = string.Empty;
 
-                switch (EnvironmentInfo.EwsApiVersion)
-                {
-                    case EwsApiVersions.OnePointZero:
-                    case EwsApiVersions.OnePointZeroHot:
-                        beginTag = "<EwsLogEntry";
-                        break;
-                    default:
-                        beginTag = "<Trace";
-                        break;
-                }
+    //            switch (EnvironmentInfo.EwsApiVersion)
+    //            {
+    //                case EwsApiVersions.OnePointZero:
+    //                case EwsApiVersions.OnePointZeroHot:
+    //                    beginTag = "<EwsLogEntry";
+    //                    break;
+    //                default:
+    //                    beginTag = "<Trace";
+    //                    break;
+    //            }
 
-                return beginTag;
-            }
-        }
+    //            return beginTag;
+    //        }
+    //    }
 
-        internal static string TraceLogEndTag
-        {
-            get
-            {
-                string endTag = string.Empty;
+    //    internal static string TraceLogEndTag
+    //    {
+    //        get
+    //        {
+    //            string endTag = string.Empty;
 
-                switch (EnvironmentInfo.EwsApiVersion)
-                {
-                    case EwsApiVersions.OnePointZero:
-                    case EwsApiVersions.OnePointZeroHot:
-                        endTag = "</EwsLogEntry>";
-                        break;
-                    default:
-                        endTag = "</Trace>";
-                        break;
-                }
+    //            switch (EnvironmentInfo.EwsApiVersion)
+    //            {
+    //                case EwsApiVersions.OnePointZero:
+    //                case EwsApiVersions.OnePointZeroHot:
+    //                    endTag = "</EwsLogEntry>";
+    //                    break;
+    //                default:
+    //                    endTag = "</Trace>";
+    //                    break;
+    //            }
 
-                return endTag;
-            }
-        }
+    //            return endTag;
+    //        }
+    //    }
 
-        /// <summary>
-        /// String that echoes the original message type given
-        /// </summary>
-        internal string OriginalTraceMessageType { get; private set; }
+    //    /// <summary>
+    //    /// String that echoes the original message type given
+    //    /// </summary>
+    //    internal string OriginalTraceMessageType { get; private set; }
 
-        /// <summary>
-        /// String that echoes the original trace message given
-        /// </summary>
-        internal string OriginalTraceMessage { get; private set; }
+    //    /// <summary>
+    //    /// String that echoes the original trace message given
+    //    /// </summary>
+    //    internal string OriginalTraceMessage { get; private set; }
 
-        internal string UniqueKey { get; private set; }
-        internal string Method { get; private set; }
+    //    internal string UniqueKey { get; private set; }
+    //    internal string Method { get; private set; }
 
-        internal EwsApiTraceParser(string type, string message)
-        {
-            this.OriginalTraceMessage = message;
-            this.OriginalTraceMessageType = type;
+    //    internal EwsApiTraceParser(string type, string message)
+    //    {
+    //        this.OriginalTraceMessage = message;
+    //        this.OriginalTraceMessageType = type;
 
             
-            this.UniqueKey = GetHeader_OnePointZero(message);
-            this.Method = GetMethod_OnePointZero(message);
+    //        this.UniqueKey = GetHeader_OnePointZero(message);
+    //        this.Method = GetMethod_OnePointZero(message);
 
-        }
+    //    }
 
-        private static string GetHeader_OnePointZero(string message)
-        {
-            return message.Substring(0, message.IndexOf(">", 0) + 1);
-        }
+    //    private static string GetHeader_OnePointZero(string message)
+    //    {
+    //        return message.Substring(0, message.IndexOf(">", 0) + 1);
+    //    }
 
-        private static string GetMethod_OnePointZero(string message)
-        {
-            string method = string.Empty;
-            if ((message.Contains("EwsResponse") || message.Contains("EwsRequest"))
-                && !message.Contains("EwsResponseHttpHeaders"))
-            {
-                if (message.Contains("<m:"))
-                {
-                    method = message.Substring(message.IndexOf("<m:")).Replace("<m:", "").Replace(">", " ");
-                    method = method.Split(' ')[0];
-                }
-            }
+    //    private static string GetMethod_OnePointZero(string message)
+    //    {
+    //        string method = string.Empty;
+    //        if ((message.Contains("EwsResponse") || message.Contains("EwsRequest"))
+    //            && !message.Contains("EwsResponseHttpHeaders"))
+    //        {
+    //            if (message.Contains("<m:"))
+    //            {
+    //                method = message.Substring(message.IndexOf("<m:")).Replace("<m:", "").Replace(">", " ");
+    //                method = method.Split(' ')[0];
+    //            }
+    //        }
 
-            return method;
-        }
+    //        return method;
+    //    }
          
-    }
+    //}
 }
